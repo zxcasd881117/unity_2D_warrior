a7729efe478a449b857c92cebfcb93e3e0f409b9
zxcasd881117 <75553486+zxcasd881117@users.noreply.github.com>
2020-12-27 16:25:38 +0800

    程式認識語法類型

---
+++ 2D_warrior/Assets/Scripts/Car.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // 單行註解 : 不會被程式讀取
+    // 欄位語法
+    // 類型 名稱;
+
+    // 四大類型
+    // 整數 int
+    // 浮點數 float
+    // 字串 string
+    // 布林值 bool
+    int height;
+    float weight;
+    string brand;
+    bool haswindow;
+
+}
