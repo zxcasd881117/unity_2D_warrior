320f81983e66c64d3a7d97c0cfef85ffc4b83735
zxcasd881117 <75553486+zxcasd881117@users.noreply.github.com>
2021-01-03 16:38:34 +0800

    方法認識方法語語法

---
+++ 2D_warrior/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 10.5f;
+    [Header("跳越高度"), Range(0, 3000)]
+    public float jump = 100;
+    [Header("是否在地板上"), Tooltip("判斷腳色是否在地面上")]
+    public bool isGrounded;
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈生成點")]
+    public Transform pointSpawn;
+    [Header("子彈速度"), Range(0, 5000)]
+    public int speedBullet = 800;
+    [Header("開槍音效")]
+    public AudioClip soundFire;
+    [Header("血量"), Range(0, 2000)]
+    public float hp = 100;
+
+    private AudioSource and;
+    private Rigidbody2D rig;
+    private Animator ani;
+}
